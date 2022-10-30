e70660177ed455459a6e58769c4d71e893a3b40f
Alex Hung <alexh4949@gmail.com>
2022-10-30 18:42:54 -0400

    blb-142 created necessary scripts for blomb

---
+++ Assets/Scripts/Entities/Enemies/Blomb.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blomb : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        IdleState = new BlombIdleState(this, StateMachine, (EnemyData)data, "Idle");
+        PatrolState = new BlombPatrolState(this, StateMachine, (EnemyData)data, "Move");
+        AlertedState = new BlombAlertedState(this, StateMachine, (EnemyData)data, "Alerted");
+        AgroState = new BlombAgroState(this, StateMachine, (EnemyData)data, "Move");
+        AttackState = new BlombAttackState(this, StateMachine, (EnemyData)data, "Attack");
+        CCState = new EnemyCCState(this, StateMachine, (EnemyData)data, "Idle");
+        DeathState = new EnemyDeathState(this, StateMachine, (EnemyData)data, "Death");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
